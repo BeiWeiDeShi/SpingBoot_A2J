2a65448c16823130d7fb6576b546ff96132fc46a
BeiWeiDeShi <1036822859@qq.com>
2023-10-28 21:35:27 +0800

    JsonController + UnserEntity

--- src/main/java/com/example/a2j_test/controller/JsonController.java
+++ src/main/java/com/example/a2j_test/controller/JsonController.java
@@ -1,17 +1,21 @@
 package com.example.a2j_test.controller;
 
+import com.example.a2j_test.beans.UserEntity;
 import com.example.a2j_test.beans.VersionEntity;
 import com.example.a2j_test.beans.base.BaseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController  // RestFul style controller layer annotation
 @RequestMapping("/app") // Request root path
 public class JsonController {
 
-    @RequestMapping(value = "/version", method = RequestMethod.GET)
     // 把方法定义成接口
+    @RequestMapping(value = "/version", method = RequestMethod.GET)
+//    @GetMapping(value = "/version")
     public BaseEntity<VersionEntity> getVersion() {
         VersionEntity version = VersionEntity.builder()
             .id(1)
@@ -22,5 +26,34 @@ public class JsonController {
 
         // 外层包装
         return BaseEntity.success(version);
+    }
+
+    /**
+     * login
+     * @param username 用户名
+     * @param password 密码
+     * @return         用户信息
+     */
+    @PostMapping(value = "/login")
+    public BaseEntity<UserEntity> login(@RequestParam String username, @RequestParam String password) {
+        if (!StringUtils.hasLength(username)) return BaseEntity.failed("username is empty!");
+        if (!StringUtils.hasLength(password)) return BaseEntity.failed("password is empty!");
+
+        // 模拟从数据库查询的结果
+        if (username.equalsIgnoreCase("hello") && password.equalsIgnoreCase("1234")) {
+            UserEntity user = UserEntity.builder()
+                    .id(1)
+                    .age(20)
+                    .email("111@qq.com")
+                    .username("hello")
+                    .password("1234")
+                    .gender(0)
+                    .createDate(new Date())
+                    .build();
+
+            return BaseEntity.success(user);
+        }
+
+        return BaseEntity.failed("username or password error !");
     }
 }

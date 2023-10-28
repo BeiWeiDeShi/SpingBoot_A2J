e2b56fe12deddbc546dafc44e067991fdbe0c947
BeiWeiDeShi <1036822859@qq.com>
2023-10-28 21:22:21 +0800

    add UserEntity.java

--- src/main/java/com/example/a2j_test/beans/UserEntity.java
+++ src/main/java/com/example/a2j_test/beans/UserEntity.java
@@ -1,4 +1,20 @@
 package com.example.a2j_test.beans;
 
-public class UserEntity {
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+public class UserEntity implements Serializable {
+
+    private Integer id;
+    private String  username;
+    private String  password;
+    private Integer age;
+    private Integer gender;
+    private String  email;
+    private Date    createDate;
 }
